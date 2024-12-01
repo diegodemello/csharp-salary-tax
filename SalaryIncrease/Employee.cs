11a6c7eaac0e501cd5005c0218887a158cd2b8b8
Diego <diegodemello027@gmail.com>
2024-12-01 15:54:43 -0300

    Adicionar arquivos de projeto.

---
+++ SalaryIncrease/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace SalaryIncrease
+{
+    internal class Employee
+    {
+        public string name;
+        public double grossSalary;
+        public double tax;
+        public double NetSalary()
+        {
+            return grossSalary - tax;
+        }
+
+        public void Increase(double percentage)
+        {
+            grossSalary = grossSalary + (grossSalary * percentage / 100.0);
+        }
+
+        public override string ToString()
+        {
+            return name 
+                + ", R$ " 
+                + NetSalary().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ SalaryIncrease/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace SalaryIncrease
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            Console.Write("Nome: ");
+            employee.name = Console.ReadLine();
+            Console.Write("Salário Bruto: ");
+            employee.grossSalary = double.Parse(Console.ReadLine());
+            Console.Write("Imposto: ");
+            employee.tax = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Funcionário: " + employee);
+
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double porcentage = double.Parse(Console.ReadLine());
+            employee.Increase(porcentage);
+
+            Console.WriteLine("Funcionário: " + employee);
+        }
+    }
+}
